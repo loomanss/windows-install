c554fb15d41f79449af72189eeaaca4bee24108d
loomanss <stijnloomans@live.be>
2021-02-25 17:20:27 +0100

    Update git_bash_alias.sh

--- scripts/git_bash_alias.sh
+++ scripts/git_bash_alias.sh
@@ -3,6 +3,6 @@
 # ff www.google.com => will open gui
 # opengit wil open remote git repository in firefox
 alias ff='/c/"Program Files (x86)"/"Mozilla Firefox"/firefox.exe'
-alias getgitremote="git remote -v | tail -1 | sed 's/.*\@/https\:\/\//g'  | egrep -o 'http.?://\S+' | sed 's/\:/\//g' | sed 's/https\/\/\//https:\/\//g'"
+alias getgitremote="git remote -v | tail -1 | sed 's/.*\@/https\:\/\//g'  | egrep -o 'http.?://\S+' | sed 's/\:/\//g2'"
 alias opengit='ff $(getgitremote)'
 alias ll=\'ls -l\'
