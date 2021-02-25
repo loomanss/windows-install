ab655dc005e690cfbedde191a097397507ca1268
loomanss <stijnloomans@live.be>
2021-02-25 20:00:01 +0100

    Update git_bash_alias.sh

--- scripts/git_bash_alias.sh
+++ scripts/git_bash_alias.sh
@@ -6,3 +6,7 @@ alias ff='/c/"Program Files (x86)"/"Mozilla Firefox"/firefox.exe'
 alias getgitremote="git remote -v | tail -1 | sed 's/.*\@/https\:\/\//g'  | egrep -o 'http.?://\S+' | sed 's/\:/\//g2'"
 alias opengit='ff $(getgitremote)'
 alias ll=\'ls -l\'
+
+
+#powershell
+# Set-Alias -Name ff -Value 'c:\"Program Files (x86)"\"Mozilla Firefox"\firefox.exe'
